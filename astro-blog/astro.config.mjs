fb2750cefbb4a7580704809b89162b534abdd784
Johannes Konings <johannes.konings@outlook.com>
2024-06-17 21:12:08 +0200

    updates

--- astro-blog/astro.config.mjs
+++ astro-blog/astro.config.mjs
@@ -3,14 +3,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-import remarkToc from "remark-toc";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://johanneskonings.dev",
-  markdown: {
-    remarkPlugins: [remarkToc],
-  },
   integrations: [
     mdx(),
     sitemap(),
