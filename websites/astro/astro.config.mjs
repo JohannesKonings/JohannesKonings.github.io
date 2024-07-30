5fee875d100af8305e3f7636fd74157779fa4119
Johannes Konings <johannes.konings@outlook.com>
2024-07-30 10:49:51 +0200

    add copy button (#34)

--- websites/astro/astro.config.mjs
+++ websites/astro/astro.config.mjs
@@ -4,12 +4,29 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import rehypeAstroRelativeMarkdownLinks from "astro-rehype-relative-markdown-links";
+import { rehypePrettyCode } from "rehype-pretty-code";
+import { transformerCopyButton } from "@rehype-pretty/transformers";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://johanneskonings.dev",
 	markdown: {
-		rehypePlugins: [rehypeAstroRelativeMarkdownLinks],
+		syntaxHighlight: false,
+		rehypePlugins: [
+			rehypeAstroRelativeMarkdownLinks,
+			[
+				rehypePrettyCode,
+				{
+					// theme: moonlightTheme,
+					transformers: [
+						transformerCopyButton({
+							visibility: "always",
+							feedbackDuration: 2_500,
+						}),
+					],
+				},
+			],
+		],
 	},
 	integrations: [
 		mdx(),
