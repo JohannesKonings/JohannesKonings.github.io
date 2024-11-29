dba729c17ea17c754722cbd084879cdaee33936f
Johannes Konings <johannes.konings@outlook.com>
2024-11-29 06:06:30 -0800

    wip

--- websites/tanstack/app.config.ts
+++ websites/tanstack/app.config.ts
@@ -1,15 +1,28 @@
-import { defineConfig } from '@tanstack/start/config'
+import { defineConfig } from "@tanstack/start/config";
 
-export const githubPagesPrefix = '/tanstack'
+export const githubPagesPrefix = "/tanstack";
 
 export default defineConfig({
-  server: {
-    compatibilityDate: '2024-11-23',
-    // prerender: {
-    //   routes: ['/'],
-    //   crawlLinks: true,
-    // },
-    static: true,
-    preset: 'static',
-  },
-})
+	// vite: {
+	//   base: githubPagesPrefix,
+	// },
+	// vite() {
+	//   return {
+	//     base: githubPagesPrefix,
+	//   }
+	// },
+	routers: {
+		client: {
+			base: githubPagesPrefix,
+		},
+	},
+	server: {
+		compatibilityDate: "2024-11-23",
+		prerender: {
+			routes: ["/"],
+			crawlLinks: true,
+		},
+		static: true,
+		preset: "static",
+	},
+});
