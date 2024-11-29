0c3583ab8eb814a00da7164eddec7e0658e1a05a
Johannes Konings <johannes.konings@outlook.com>
2024-11-28 18:56:52 -0800

    update

--- websites/tanstack/app/routes/index.tsx
+++ websites/tanstack/app/routes/index.tsx
@@ -1,7 +1,22 @@
 // app/routes/index.tsx
+import * as React from "react";
 import * as fs from "node:fs";
 import { createFileRoute, useRouter } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/start";
+import {
+	ThemeProvider,
+	createTheme,
+	CssBaseline,
+	Container,
+	Typography,
+	Link,
+} from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faTwitter,
+	faGithub,
+	faBluesky,
+} from "@fortawesome/free-brands-svg-icons";
 
 const filePath = "count.txt";
 
@@ -29,20 +44,45 @@ export const Route = createFileRoute("/")({
 	loader: async () => await getCount(),
 });
 
+const darkTheme = createTheme({
+	palette: {
+		mode: "dark",
+	},
+});
+
 function Home() {
 	const router = useRouter();
 	const state = Route.useLoaderData();
 
 	return (
-		<button
-			type="button"
-			onClick={() => {
-				updateCount({ data: 1 }).then(() => {
-					router.invalidate();
-				});
-			}}
-		>
-			Add 1 to {state}?
-		</button>
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<Container style={{ textAlign: "center" }}>
+				<img src="../../img/avatar.png" alt="Avatar" />
+				<div>
+					<Link
+						href="https://github.com/johannes"
+						target="_blank"
+						rel="noopener noreferrer"
+						color="inherit"
+						style={{ margin: "0 10px" }}
+					>
+						<FontAwesomeIcon icon={faGithub} size="2x" />
+					</Link>
+					<Link
+						href="https://bsky.app/profile/johanneskonings.dev"
+						target="_blank"
+						rel="noopener noreferrer"
+						color="inherit"
+						style={{ margin: "0 10px" }}
+					>
+						<FontAwesomeIcon icon={faBluesky} size="2x" />
+					</Link>
+				</div>
+				<Typography variant="h6" style={{ fontFamily: "Cascadia Code" }}>
+					Johannes Konings
+				</Typography>
+			</Container>
+		</ThemeProvider>
 	);
 }
