26c4075d73d6fc4aecf0321e76d8f8f3b2f1e650
Johannes Konings <johannes.konings@outlook.com>
2024-11-28 20:12:47 -0800

    wip

--- websites/tanstack/app/router.tsx
+++ websites/tanstack/app/router.tsx
@@ -4,7 +4,7 @@ import { routeTree } from "./routeTree.gen";
 
 export function createRouter() {
 	const router = createTanStackRouter({
-		// basepath: "/tanstack",
+		basepath: "/tanstack",
 		routeTree,
 	});
 
