47c1704938373c66455a444271dbc0fef2488a8b
Johannes Konings <johannes.konings@outlook.com>
2024-11-28 20:02:12 -0800

    wip

--- websites/tanstack/app/routes/index.tsx
+++ websites/tanstack/app/routes/index.tsx
@@ -1,48 +1,51 @@
 // app/routes/index.tsx
-import * as React from 'react'
-import * as fs from 'node:fs'
-import { createFileRoute, useRouter } from '@tanstack/react-router'
-import { createServerFn } from '@tanstack/start'
+import * as React from "react";
+import * as fs from "node:fs";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/start";
 import {
-  ThemeProvider,
-  createTheme,
-  CssBaseline,
-  Container,
-  Typography,
-  Link,
-} from '@mui/material'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+	ThemeProvider,
+	createTheme,
+	CssBaseline,
+	Container,
+	Typography,
+	Link,
+} from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faTwitter,
-  faGithub,
-  faBluesky,
-} from '@fortawesome/free-brands-svg-icons'
+	faTwitter,
+	faGithub,
+	faBluesky,
+} from "@fortawesome/free-brands-svg-icons";
+import avatar from "../images/avatar.png";
 
-const filePath = 'count.txt'
+const filePath = "count.txt";
 
 async function readCount() {
-  return parseInt(
-    await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
-  )
+	return parseInt(
+		await fs.promises.readFile(filePath, "utf-8").catch(() => "0"),
+	);
 }
 
 const getCount = createServerFn({
-  method: 'GET',
+	method: "GET",
 }).handler(() => {
-  return readCount()
-})
+	return readCount();
+});
 
-const updateCount = createServerFn({ method: 'POST' })
-  .validator((d: number) => d)
-  .handler(async ({ data }) => {
-    const count = await readCount()
-    await fs.promises.writeFile(filePath, `${count + data}`)
-  })
+const updateCount = createServerFn({ method: "POST" })
+	.validator((d: number) => d)
+	.handler(async ({ data }) => {
+		const count = await readCount();
+		await fs.promises.writeFile(filePath, `${count + data}`);
+	});
 
-export const Route = createFileRoute('/')({
-  component: Home,
-  loader: async () => await getCount(),
-})
+export const Route = createFileRoute("/")({
+	component: React.lazy(() =>
+		import("./index").then((module) => ({ default: module.Home })),
+	),
+	loader: async () => await getCount(),
+});
 // const rootRoute = createRootRoute({
 // });
 // export const Route = createRoute({
@@ -52,44 +55,45 @@ export const Route = createFileRoute('/')({
 // });
 
 const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-})
+	palette: {
+		mode: "dark",
+	},
+});
 
-function Home() {
-  const router = useRouter()
-  const state = Route.useLoaderData()
+export function Home() {
+	const router = useRouter();
+	const state = Route.useLoaderData();
 
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Container style={{ textAlign: 'center' }}>
-        <img src="../../img/avatar.png" alt="Avatar" />
-        <div>
-          <Link
-            href="https://github.com/johanneskonings"
-            target="_blank"
-            rel="noopener noreferrer"
-            color="inherit"
-            style={{ margin: '0 10px' }}
-          >
-            <FontAwesomeIcon icon={faGithub} size="2x" />
-          </Link>
-          <Link
-            href="https://bsky.app/profile/johanneskonings.dev"
-            target="_blank"
-            rel="noopener noreferrer"
-            color="inherit"
-            style={{ margin: '0 10px' }}
-          >
-            <FontAwesomeIcon icon={faBluesky} size="2x" />
-          </Link>
-        </div>
-        <Typography variant="h3" style={{ fontFamily: 'Cascadia Code' }}>
-          Johannes Konings
-        </Typography>
-      </Container>
-    </ThemeProvider>
-  )
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<Container style={{ textAlign: "center" }}>
+				{/* <img src="/tanstack/img/avatar.png" alt="Avatar" /> */}
+				<img src={avatar} alt="Avatar" />
+				<div>
+					<Link
+						href="https://github.com/johanneskonings"
+						target="_blank"
+						rel="noopener noreferrer"
+						color="inherit"
+						style={{ margin: "0 10px" }}
+					>
+						<FontAwesomeIcon icon={faGithub} size="2x" />
+					</Link>
+					<Link
+						href="https://bsky.app/profile/johanneskonings.dev"
+						target="_blank"
+						rel="noopener noreferrer"
+						color="inherit"
+						style={{ margin: "0 10px" }}
+					>
+						<FontAwesomeIcon icon={faBluesky} size="2x" />
+					</Link>
+				</div>
+				<Typography variant="h3" style={{ fontFamily: "Cascadia Code" }}>
+					Johannes Konings
+				</Typography>
+			</Container>
+		</ThemeProvider>
+	);
 }
