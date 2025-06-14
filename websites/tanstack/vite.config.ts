761c3ff12f26850a6ae016113968644c57658717
Johannes Konings <johannes.konings@outlook.com>
2025-06-14 09:08:09 -0400

    add base path

--- websites/tanstack/vite.config.ts
+++ websites/tanstack/vite.config.ts
@@ -3,6 +3,7 @@ import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
+	base: "/tanstack/", // Added base path
 	server: {
 		port: 3000,
 	},
