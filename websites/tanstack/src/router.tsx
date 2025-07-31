15c07ee3e0268007dc7e0226277c1f004f758eda
Johannes Konings <johannes.konings@outlook.com>
2025-07-31 11:43:12 +0200

    wip

--- websites/tanstack/src/router.tsx
+++ websites/tanstack/src/router.tsx
@@ -4,7 +4,7 @@ import { routeTree } from "./routeTree.gen";
 
 export function createRouter() {
 	const router = createTanStackRouter({
-		basepath: process.env.NODE_ENV === "production" ? "/tanstack" : undefined, // Use /tanstack for production (GitHub Pages)
+		basepath: import.meta.env.PROD ? "/tanstack" : undefined, // Use /tanstack for production (GitHub Pages)
 		routeTree,
 		scrollRestoration: true,
 	});
