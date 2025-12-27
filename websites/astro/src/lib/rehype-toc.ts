3d7cf61afcab326038676b7258efb28fde7c3647
Johannes Konings <johannes.konings@outlook.com>
2025-12-27 20:08:28 +0100

    add post tanstack db multiple entities

---
+++ websites/astro/src/lib/rehype-toc.ts
@@ -0,0 +1,10 @@
+/**
+ * Generate a slug from heading text
+ */
+export function generateSlug(text: string): string {
+	return text
+		.toLowerCase()
+		.replace(/[^\w\s-]/g, "")
+		.replace(/\s+/g, "-")
+		.replace(/^-+|-+$/g, "");
+}
